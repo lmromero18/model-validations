ce83cf5f56ab0874c8d6b221f347a72ed6033ce9
lmromero18 <lmromero.18@est.ucab.edu.ve>
2025-02-15 10:13:51 -0400

    Ajustes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ericcor/model-validations',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '005df9601608a5206773f7e6999bc19cb6115694',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ericcor/model-validations' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '005df9601608a5206773f7e6999bc19cb6115694',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
